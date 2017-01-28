20f0f6d0bbe4bd126e675bae29286650ffb751f7
VladP97 <vlad.potapov1997@gmail.com>
2017-01-28 16:06:44 +0300

    guessing-game/feature: binary search algorithm

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,38 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+        this.prevMethod = " ";
+        this.guessVal = 0;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+      if (this.prevMethod == " ")
+      {
+        this.guessVal = Math.ceil((this.min+this.max)/2);
+      }
+       if (this.prevMethod == "lower"){
+         this.max = this.guessVal;
+         this.guessVal = Math.ceil((this.min+this.max)/2);
+       }
+       if (this.prevMethod == "greater"){
+         this.min = Math.ceil((this.min+this.max)/2);
+         this.guessVal = Math.ceil((this.min+this.max)/2);
+       }
+       return this.guessVal;
     }
 
     lower() {
-
+       this.prevMethod = "lower";
     }
 
     greater() {
-
+       this.prevMethod = "greater";
     }
 }
 
